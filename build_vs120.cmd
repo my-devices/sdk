1ae3e85cd6a970586e5d36657fd832d03ab32056
Guenter Obiltschnig <guenter.obiltschnig@appinf.com>
2016-08-31 15:04:27 +0200

    fixed Windows build scripts

--- build_vs120.cmd
+++ build_vs120.cmd
@@ -1,4 +1,4 @@
 @echo off
 if defined VS120COMNTOOLS (
 call "%VS120COMNTOOLS%\vsvars32.bat")
-buildwin 120 build shared both Win32 samples
+buildwin 120 build static_mt release Win32 nosamples
--- build_vs90.cmd
+++ build_vs90.cmd
@@ -1,4 +1,4 @@
 @echo off
 if defined VS90COMNTOOLS (
 call "%VS90COMNTOOLS%\vsvars32.bat")
-buildwin 90 build shared both Win32 samples
+buildwin 90 build static_mt release Win32 nosamples
